8f6ac0b46de514750fb39c6e162fbe48a7d9d1c7
Alex Tverdyy <alextverdyy@gmail.com>
2017-01-23 10:53:01 +0100

    Commit

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$article->title}}</title>
+</head>
+<body>
+    <h1>{{$article->title}}</h1>
+    <hr>
+    {{$article->content}}
+    <hr>
+    {{$article->user->name}} | {{$article->category->name}} |
+    @foreach($article->tag as $tag)
+        {{$tag->name}}
+
+    @endforeach
+</body>
+</html>
+
+
+
+
